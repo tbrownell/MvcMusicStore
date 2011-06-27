056d598b51c89c64d80a5c3f53e02280facae353
Terry Brownell <tbrownell1@gmail.com>
2011-06-27 16:48:44 -0600

    On Page 95 of the tutorial. Next up ViewModels

--- MvcMusicStore/Models/MusicStoreEntities.cs
+++ MvcMusicStore/Models/MusicStoreEntities.cs
@@ -10,7 +10,8 @@ namespace MvcMusicStore.Models
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
-
         public DbSet<Artist> Artists { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
